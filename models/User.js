70e202a069831387241c5b4f2da8647cab013631
Jason Serafica <seraficaj@gmail.com>
2023-05-10 16:11:53 -0700

    add password hashing and bcrypt to user model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+});
+
+// Hash the user's password before saving it to DB
+userSchema.pre("save", async function (next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(this.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
 });
 
 mongoose.model("User", userSchema);
